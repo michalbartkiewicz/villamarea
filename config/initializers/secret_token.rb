a4ead7ead8a69db47fd93c875a199d16efada8f8
Michal Bartkiewicz <michal.bartkiewicz@gmail.com>
2011-06-08 10:26:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Villamarea::Application.config.secret_token = '6231ea065d8f6c7ec76954a2ce536da3b6ab313935c790e8db37ecaeb668be70c91b3b46eb62ea41f1dfdeefd3243c48f737de3e86dd0e23b1702504fe7eea76'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Villamarea::Application.config.session_store :cookie_store, :key => '_villamarea_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Villamarea::Application.config.session_store :active_record_store
